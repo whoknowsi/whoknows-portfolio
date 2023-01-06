233a1ab98e0e8084f1cf6d99583422ed0a565328
whoknowsi <echosmania@gmail.com>
2023-01-06 05:20:52 -0300

    Added main component to contact

---
+++ components/Contact/Main.js
@@ -0,0 +1,12 @@
+import styles from '../../styles/components/Contact/Main.module.css'
+import Card from './Card'
+
+const Main = () => {
+  return (
+    <main className={styles.container}>
+      <Card />
+    </main>
+  )
+}
+
+export default Main

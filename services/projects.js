54a6a42c01f2c533a7466f1c6d90169b4775c728
whoknowsi <echosmania@gmail.com>
2023-01-07 12:33:43 -0300

    removing console.lgo

--- services/projects.js
+++ services/projects.js
@@ -3,7 +3,6 @@ const url = `${process.env.API_BASE_URL}/projects`
 const getLastProjects = async () => {
   const response = await fetch(url)
   const data = await response.json()
-  console.log(data)
   return data.results
     ? data.results
     : []

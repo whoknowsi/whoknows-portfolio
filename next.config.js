d1eb7330b04635d87bfec49f27c58cff9360913f
whoknowsi <echosmania@gmail.com>
2023-01-08 01:26:35 -0300

    fixed not env url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    API_BASE_URL: 'https://personal-data-api.vercel.app'
+  }
 }
 
 module.exports = nextConfig

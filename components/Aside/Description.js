7652e8b201bdd0f2c7abba7da444fa229d6ad330
whoknowsi <echosmania@gmail.com>
2023-01-09 16:43:57 -0300

    Added link to home on my name

--- components/Aside/Description.js
+++ components/Aside/Description.js
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import styles from '../../styles/components/Aside/Description.module.css'
 
 const Description = ({ name, lastName, descriptions }) => {
   return (
     <div className={styles.container}>
-      <h2>{`${name} ${lastName}`}</h2>
+      <Link href='/'>
+        <h2>{`${name} ${lastName}`}</h2>
+      </Link>
       {descriptions.map((description) => (
         <p key={description}>{description}</p>
       ))}

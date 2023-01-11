4f430412e8582ab495251f738e971e1631604123
whoknowsi <echosmania@gmail.com>
2023-01-11 17:43:27 -0300

    Start creating certificates page

---
+++ services/certificates.js
@@ -0,0 +1,11 @@
+const url = `${process.env.API_BASE_URL}/certificates`
+
+const getCertificates = async () => {
+  const response = await fetch(url)
+  const data = await response.json()
+  return data.results
+    ? data.results
+    : []
+}
+
+export { getCertificates }

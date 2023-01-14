97d4d44098ea10bb0dace03448cc0b08d1765fd4
whoknowsi <echosmania@gmail.com>
2023-01-14 20:47:28 -0300

    Removing log

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,6 @@
 export default async function handler (req, res) {
   require('dotenv').config()
   const nodemailer = require('nodemailer')
-  console.log(process.env)
   const transporter = nodemailer.createTransport({
     port: 465,
     host: 'smtp.gmail.com',

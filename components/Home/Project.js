325c329d16c0939a3d63ccf85641edc387f0af11
whoknowsi <echosmania@gmail.com>
2023-01-15 01:35:18 -0300

    Adding placeholder on home last projects

--- components/Home/Project.js
+++ components/Home/Project.js
@@ -29,23 +29,7 @@ const Project = ({ name, description, createdAt, status, client, media, _id }) =
     </div>
       )
     : (
-      <div className={styles.containerPlaceHolder}>
-        <Link href={`/projects/${_id}`}><h3>Placeholder</h3></Link>
-        <div className={styles.imageContainer}>
-          <div className={styles.imagePlaceHolder}></div>
-        </div>
-        <p>
-          Donec quis ante lorem. Aenean at orci vehicula, vehicula mauris at, euismod augue.
-          Integer tincidunt odio eu metus feugiat rhoncus. Vivamus rutrum, nulla non imperdiet
-          sodales, mi enim scelerisque mauris, malesuada tempor lorem quam eu enim. Proin ornare
-          commodo ligula, sed posuere ante cursus sed. Nulla risus enim, porta sit amet pulvinar
-          vitae, tincidunt sit amet massa. Nulla aliquam enim nec justo tempor malesuada. Donec
-          ac molestie ipsum, nec lacinia elit. Suspendisse vulputate massa eget ex mollis, at
-          eleifend tellus fringilla. Aenean rutrum faucibus nunc, quis vestibulum urna feugiat at.
-          Vestibulum non blandit massa. Donec fermentum ultricies ligula in tempus. Proin ullamcorper,
-          sem in vehicula rutrum, leo libero consequat sapien, et imperdiet risus ante eget sapien.
-        </p>
-      </div>
+      <div className={styles.containerPlaceHolder}></div>
       )
 }
 

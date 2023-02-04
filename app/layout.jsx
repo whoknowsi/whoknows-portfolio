02c11cf3557f5376f129018f9ce9cf7ca1e09830
“Your <echosmania@gmail.com>
2023-02-04 19:04:18 -0300

    Added poppins font with next/font

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,12 +12,16 @@ import Head from './head'
 import Navigation from './components/Navigation'
 import { getBasicInfo } from '@/services/basic-info'
 import { getProjects } from '@/services/projects'
+
+import { Poppins } from '@next/font/google'
+const poppins = Poppins({ weight: ['300', '400', '600'], subsets: ['latin'] })
+
 export default async function RootLayout ({ children }) {
   const basicInfo = await getBasicInfo()
   const projects = await getProjects()
 
   return (
-    <html lang="en">
+    <html className={poppins.className} lang="en">
       <Head />
       <body>
         <div className='container'>

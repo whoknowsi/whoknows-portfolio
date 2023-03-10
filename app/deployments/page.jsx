753b0476c51f38452e6f8ac502cdf3d0ae5562be
whoknowsi <echosmania@gmail.com>
2023-03-10 19:17:24 -0300

    Fetching deployments from client

--- app/deployments/page.jsx
+++ app/deployments/page.jsx
@@ -1,19 +1,34 @@
-import { use } from 'react'
+'use client'
+
 import { getDeployments } from './services/deployments'
 import DeploymentsElement from './components/DeploymentsElement'
 import Head from '@/app/head'
 import Footer from '../components/Footer/Footer'
+import { useEffect, useState } from 'react'
+import Loading from '../loading'
 
-export default function Deployments () {
-  const deployments = use(getDeployments())
+export default function Deployments() {
+  const [deployments, setDeployments] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      setDeployments(await getDeployments())
+    })()
+  }, [])
 
   return (
     <>
       <Head>
         <title>Whoknows | Deployments</title>
       </Head>
-      <DeploymentsElement deployments={deployments}/>
-      <Footer />
+      {deployments.length > 0 ? (
+        <>
+          <DeploymentsElement deployments={deployments} />
+          <Footer />
+        </>
+      ) : (
+        <Loading />
+      )}
     </>
   )
 }
--- app/deployments/services/deployments.js
+++ app/deployments/services/deployments.js
@@ -1,18 +1,13 @@
-const url = 'https://personal-data-api.vercel.app/deployments'
+import deployments from '@/data/deployments.json'
 const checkApi = 'https://is-alive.whoknows.workers.dev/check'
 
 export async function getDeployments() {
-  const response = await fetch(url, { cache: 'no-store' })
-  const data = await response.json()
-
   const finalData = []
 
-  if (data.results) {
-    for (const deployment of data.results) {
-      const response = await fetch(`${checkApi}?url=${deployment.url}`, { cache: 'no-store' })
-      const data = await response.json()
-      finalData.push({ ...deployment, status: data.message })
-    }
+  for (const deployment of deployments) {
+    const response = await fetch(`${checkApi}?url=${deployment.url}`, { cache: 'no-store' })
+    const data = await response.json()
+    finalData.push({ ...deployment, status: data.message })
   }
 
   return finalData

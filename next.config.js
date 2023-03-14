863633377be65b16b3a2e1e9dacc6efc04e8bb7e
whoknowsi <echosmania@gmail.com>
2023-03-14 15:00:32 -0300

    Using deployed api to deploy

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   env: {
     STATICS_BASE_URL: 'https://personal-data-api.netlify.app',
-    API_BASE_URL: 'http://localhost:3001/.netlify/functions/api',
+    API_BASE_URL: 'https://personal-data-api.netlify.app/.netlify/functions/api',
     API_CHECK_URL: 'https://is-alive.whoknows.workers.dev/check'
   },
   images: {
